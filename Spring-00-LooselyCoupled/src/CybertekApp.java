e290951f213f9cbc337f56cb1bc6065ee1818faf
nihalkaftan <nihalkaftan@gmail.com>
2020-10-28 23:42:48 -0400

    Loosely Coupled updated

--- Spring-00-LooselyCoupled/src/CybertekApp.java
+++ Spring-00-LooselyCoupled/src/CybertekApp.java
@@ -1,12 +1,13 @@
-import implemetation.Mentor;
 import service.FullTimeMentor;
 import service.MentorAccounts;
 
 public class CybertekApp {
     public static void main(String[] args) {
+        //PartTimeMentor partTime = new PartTimeMentor();
         FullTimeMentor fullTime = new FullTimeMentor();
         MentorAccounts mentor = new MentorAccounts(fullTime);
         mentor.manageAccount();
+
 
     }
 }
