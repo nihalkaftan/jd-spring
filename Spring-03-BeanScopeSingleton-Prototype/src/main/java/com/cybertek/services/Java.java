5244dd95adcd9e5f20afdf2cef8ce72f3fd52f80
McMillerErika <erikamcmiller65@gmail.com>
2020-10-29 22:31:30 -0400

    Bean Scope Singleton Prototype

--- Spring-03-BeanScopeSingleton-Prototype/src/main/java/com/cybertek/services/Java.java
+++ Spring-03-BeanScopeSingleton-Prototype/src/main/java/com/cybertek/services/Java.java
@@ -1,25 +1,14 @@
 package com.cybertek.services;
 
 import com.cybertek.interfaces.Course;
-import com.cybertek.interfaces.ExtraSessions;
 import lombok.Data;
 
 @Data
 public class Java implements Course {
 
-    private ExtraSessions extraSessions;
-/**
-    public OfficeHours getOfficeHours() {
-        return officeHours;
-    }
-
-    public void setOfficeHours(OfficeHours officeHours) { //set + officeHours
-        this.officeHours = officeHours;
-    }
- */
 
     public void getTeachingHours() {
-        System.out.println("Weekly teaching hours : " + (20 + extraSessions.getHours()));
+        System.out.println("Weekly teaching hours : 20");
 
     }
 }
