148c8469425a54659baf483a34811e9b2416a524
McMillerErika <erikamcmiller65@gmail.com>
2020-10-29 21:30:24 -0400

    constructor arg and dependency injection

--- Spring-02-DI-ConstructorInjection/src/main/java/com/cybertek/services/Java.java
+++ Spring-02-DI-ConstructorInjection/src/main/java/com/cybertek/services/Java.java
@@ -3,7 +3,14 @@ package com.cybertek.services;
 import com.cybertek.interfaces.Course;
 
 public class Java implements Course {
+
+    OfficeHours officeHours;
+
+    public Java(OfficeHours officeHours) {
+        this.officeHours = officeHours;
+    }
+
     public void getTeachingHours() {
-        System.out.println("Weekly teaching hours : 25");
+        System.out.println("Weekly teaching hours : " + (20 + officeHours.getHours()));
     }
 }
