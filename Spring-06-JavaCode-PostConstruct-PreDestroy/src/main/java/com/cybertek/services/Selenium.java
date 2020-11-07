55a6c76ed30e005ef21e2e1aaa7432dc25b3e913
McMillerErika <erikamcmiller65@gmail.com>
2020-11-07 17:40:42 -0500

    Java Annotations - PostConstruct adn PreDestroy

--- Spring-06-JavaCode-PostConstruct-PreDestroy/src/main/java/com/cybertek/services/Selenium.java
+++ Spring-06-JavaCode-PostConstruct-PreDestroy/src/main/java/com/cybertek/services/Selenium.java
@@ -4,6 +4,7 @@ import com.cybertek.interfaces.Course;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 
 @Component
 public class Selenium implements Course {
@@ -14,5 +15,10 @@ public class Selenium implements Course {
     @PostConstruct
     public void postConstruct(){
         System.out.println("Executing post construct");
+    }
+
+    @PreDestroy
+    public void preDestroy(){
+        System.out.println("Executing pre destroy");
     }
 }
