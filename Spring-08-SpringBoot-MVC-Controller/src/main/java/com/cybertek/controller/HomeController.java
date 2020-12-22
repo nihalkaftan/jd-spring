8d73682aa4007c96ad8bc166e5d6d664f51c6a12
nihalkaftan <nihalkaftan@gmail.com>
2020-12-21 21:47:17 -0500

    SpringBoot

--- Spring-08-SpringBoot-MVC-Controller/src/main/java/com/cybertek/controller/HomeController.java
+++ Spring-08-SpringBoot-MVC-Controller/src/main/java/com/cybertek/controller/HomeController.java
@@ -50,8 +50,8 @@ public class HomeController {
         return "home";
     }
 
-    @GetMapping(value="home/course")
-    public String requestParamEx2(@RequestParam(value="name", required = false, defaultValue = "Cybertek") String name ){
+    @GetMapping(value="home/course2")
+    public String requestParamEx2(@RequestParam(value="course2", required = false, defaultValue = "Cybertek") String name ){
         System.out.println("name is: " + name);
         return "home";
     }
---
+++ Spring-08-SpringBoot-MVC-Controller/src/main/java/com/cybertek/controller/MentorController.java
@@ -0,0 +1,13 @@
+package com.cybertek.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("mentor")
+public class MentorController {
+    @RequestMapping("/show")
+    public String showForm(){
+        return "home";
+    }
+}
