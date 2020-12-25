f08c521882ae2039f0f457f2f6911d1ba5bffea4
nihalkaftan <nihalkaftan@gmail.com>
2020-12-24 21:59:36 -0500

    mvc model and view with thymeleaf

--- Spring-08-SpringBoot-MVC-Controller/src/main/java/com/cybertek/controller/MentorController.java
+++ Spring-08-SpringBoot-MVC-Controller/src/main/java/com/cybertek/controller/MentorController.java
@@ -1,6 +1,7 @@
 package com.cybertek.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
