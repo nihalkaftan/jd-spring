3e6c33bf8296557e40ea3964800a8c2ab173bcd0
nihalkaftan <nihalkaftan@gmail.com>
2020-12-25 16:08:07 -0500

    updated mentor and created employee

--- Spring-08-SpringBoot-MVC-Form/src/main/java/com/cybertek/controller/MentorController.java
+++ Spring-08-SpringBoot-MVC-Form/src/main/java/com/cybertek/controller/MentorController.java
@@ -4,6 +4,8 @@ import com.cybertek.model.Mentor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Arrays;
@@ -18,5 +20,11 @@ public class MentorController {
         List<String> batchList = Arrays.asList("B7","B8","B9","B10","B11","B12","B13","B14","B15");
         model.addAttribute("batchList", batchList);
         return "mentor/mentor-register";
+    }
+
+    @PostMapping("/confirm")
+    public String submitForm(@ModelAttribute("mentor") Mentor mentorNihal){
+        System.out.println(mentorNihal.toString());
+        return "/mentor/mentor-confirmation";
     }
 }
