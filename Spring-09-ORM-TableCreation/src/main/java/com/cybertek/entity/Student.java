204c1c73620357238908a4bc7cca2edbe5888505
nihalkaftan <nihalkaftan@gmail.com>
2021-01-12 23:10:03 -0500

    SQL and ORM review

--- Spring-09-ORM-TableCreation/src/main/java/com/cybertek/entity/Student.java
+++ Spring-09-ORM-TableCreation/src/main/java/com/cybertek/entity/Student.java
@@ -1,9 +1,12 @@
 package com.cybertek.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import com.cybertek.enums.Gender;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.Date;
 
 @Entity
 public class Student {
@@ -12,7 +15,30 @@ public class Student {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long studentID;
 
+    @Column(name = "studentFirstName")
     private String firstName;
+    @Column(name = "studentLastName")
     private String lastName;
+    @Column(name = "studentEmailAddress")
     private String email;
+
+    @Transient
+    private String city;
+
+    @Temporal(TemporalType.DATE)
+    private Date birthDate;
+    @Temporal(TemporalType.TIME)
+    private Date birthTime;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date birthDateTime;
+
+    @Column(columnDefinition = "DATE")
+    private LocalDate localDate;
+    @Column(columnDefinition = "TIME")
+    private LocalTime locateTime;
+    @Column(columnDefinition = "TIMESTAMP")
+    private LocalDateTime localDateTime;
+
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
 }
