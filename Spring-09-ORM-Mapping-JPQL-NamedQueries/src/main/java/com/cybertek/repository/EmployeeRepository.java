95f68b457cf50c1a319f0af80a38a6e1a01d1543
nihalkaftan <nihalkaftan@gmail.com>
2021-01-17 14:21:10 -0500

    Named Queries with single and multiple binding

--- Spring-09-ORM-Mapping-JPQL-NamedQueries/src/main/java/com/cybertek/repository/EmployeeRepository.java
+++ Spring-09-ORM-Mapping-JPQL-NamedQueries/src/main/java/com/cybertek/repository/EmployeeRepository.java
@@ -3,8 +3,10 @@ package com.cybertek.repository;
 import com.cybertek.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -16,12 +18,20 @@ public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
     @Query("SELECT e.salary FROM Employee e WHERE e.email='npointona@vistaprint.com'")
     Integer getEmployeeSalary();
 
-    //single bind parameter
+    //single position bind parameter
     @Query("SELECT e FROM Employee e WHERE e.email=?1")
     Optional<Employee> getEmployeeByEmail(String email);
 
-    //multiple bind parameter
+    //multiple position bind parameter
     @Query("SELECT e FROM Employee e WHERE e.email=?1 AND e.salary=?2")
     Employee getEmployeeByEmailAndSalary(String email, int salary);
+
+    //single bind named parameter
+    @Query("SELECT e FROM Employee e WHERE e.salary=:salary")
+    Employee getEmployeeBySalary(@Param("salary")int salary);
+
+    //multiple bind named parameter
+    @Query("SELECT e FROM Employee e WHERE e.firstName=:name OR e.salary=:salary")
+    List<Employee> getEmployeeByFirstNameOrSalary(@Param("name")String firstNme, @Param("salary")int salary);
 
 }
